228abf6ef4007a4fabd7e8e00648a2464a09e9f1
Ernani da Costa <ernanidacosta@gmail.com>
2020-05-29 13:53:45 -0300

    Implementado Biblioteca Mock

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,3 +1,5 @@
+from unittest.mock import Mock
+
 import pytest
 
 from libpythonpro.spam.enviador_de_email import Enviador
@@ -20,14 +22,14 @@ from libpythonpro.spam.modelos import Usuario
 def test_quantidade_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'ernanidacosta@gmail.com',
         'Aula Python Pro',
         'Modulo PyTools'
     )
-    assert len(usuarios) == enviador.quantidade_emails_enviados
+    assert len(usuarios) == enviador.enviar.call_count
 
 
 class EnviadorMock(Enviador):
@@ -45,14 +47,14 @@ class EnviadorMock(Enviador):
 def test_parametros_de_spam(sessao):
     usuario = Usuario(nome='Ernani', email='kakizon@gmail.com')
     sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'ernanidacosta@gmail.com',
         'Aula Python Pro',
         'Modulo PyTools'
     )
-    assert enviador.parametros_de_envio == (
+    enviador.enviar.assert_called_once_with (
         'ernanidacosta@gmail.com',
         'kakizon@gmail.com',
         'Aula Python Pro',
