03cef6982e484066da92de6dc4e902121640d8ec
Ernani da Costa <ernanidacosta@gmail.com>
2020-05-29 03:18:06 -0300

    alguns testes implementados

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,11 +1,60 @@
+import pytest
+
 from libpythonpro.spam.enviador_de_email import Enviador
 from libpythonpro.spam.main import EnviadorDeSpam
+from libpythonpro.spam.modelos import Usuario
 
 
-def test_envio_de_spam(sessao):
-    enviador_de_spam = EnviadorDeSpam(sessao, Enviador())
+@pytest.mark.parametrize(
+    'usuarios',
+    [
+        [
+            Usuario(nome='Ernani', email='kakizon@gmail.com'),
+            Usuario(nome='Nito', email='kakizon@gmail.com')
+        ],
+        [
+            Usuario(nome='Ernani', email='kakizon@gmail.com')
+        ]
+    ]
+)
+def test_quantidade_de_spam(sessao, usuarios):
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    enviador = EnviadorMock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'ernanidacosta@gmail.com',
+        'Aula Python Pro',
+        'Modulo PyTools'
+    )
+    assert len(usuarios) == enviador.quantidade_emails_enviados
+
+
+class EnviadorMock(Enviador):
+
+    def __init__(self):
+        super().__init__()
+        self.quantidade_emails_enviados = 0
+        self.parametros_de_envio = None
+
+    def enviar(self, remetente, destinatario, assunto, corpo):
+        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
+        self.quantidade_emails_enviados += 1
+
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome='Ernani', email='kakizon@gmail.com')
+    sessao.salvar(usuario)
+    enviador = EnviadorMock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        'ernanidacosta@gmail.com',
+        'Aula Python Pro',
+        'Modulo PyTools'
+    )
+    assert enviador.parametros_de_envio == (
+        'ernanidacosta@gmail.com',
+        'kakizon@gmail.com',
         'Aula Python Pro',
         'Modulo PyTools'
     )
--- libpythonpro/tests/test_spam/test_usuario.py
+++ libpythonpro/tests/test_spam/test_usuario.py
@@ -2,14 +2,14 @@ from libpythonpro.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='Ernani')
+    usuario = Usuario(nome='Ernani', email='kakizon@gmail.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 
 def test_listar_usuario(conexao, sessao):
-    usuarios = [Usuario(nome='Ernani'), Usuario(nome='Nito')]
+    usuarios = [Usuario(nome='Ernani', email='kakizon@gmail.com'), Usuario(nome='Nito', email='kakizon@gmail.com')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
