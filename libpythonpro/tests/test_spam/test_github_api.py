58adcb04fce05e3d2337819f4713367f8a7679e5
Ernani da Costa <ernanidacosta@gmail.com>
2020-05-30 03:06:13 -0300

    correcoes

--- libpythonpro/tests/test_spam/test_github_api.py
+++ libpythonpro/tests/test_spam/test_github_api.py
@@ -7,8 +7,8 @@ def test_buscar_avatar():
     resp_mock = Mock()
     resp_mock.json.return_value = {
         'login': 'ernanidacosta', 'id': 11460643,
-        'avater_url': 'https://avatars3.githubusercontent.com/u/11460643?v=4',
+        'avatar_url': 'https://avatars3.githubusercontent.com/u/11460643?v=4',
     }
     github_api.requests.get = Mock(return_value=resp_mock)
-    url = github_api.buscar_avatar('ernanidacosta ')
+    url = github_api.buscar_avatar('ernanidacosta')
     assert 'https://avatars3.githubusercontent.com/u/11460643?v=4' == url
